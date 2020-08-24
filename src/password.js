3cc53ecb20538367d5b351807bcf73762b6e11fd
jefiedler <jonas.fiedler@magenta.de>
2020-08-24 22:10:31 +0200

    Create password.js with fs readfile

---
+++ src/password.js
@@ -0,0 +1,12 @@
+const { async } = require("rxjs");
+
+const fs = require("fs").promises;
+
+async function readPassword(key) {
+    const passwordJson = await fs.readFile("./password.json", "utf-8");
+    const passwords = JSON.parse(passwordJson);
+    const password = password[key];
+    return password;
+}
+
+exports.readPassword = readPassword;
