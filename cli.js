164e05f2130b9ff5b1d1f41c48a3fd8d60f363f7
jefiedler <jonas.fiedler@magenta.de>
2020-08-24 10:45:57 +0200

    Input with inquirer

--- cli.js
+++ cli.js
@@ -1,9 +1,33 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
+// Password input in console with board moduls
+
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   })
   
-  readline.question(`What's your password?`, password => {
-    console.log(`Your password is ${password}!`)
-    readline.close()
-  })
+//   readline.question(`What's your password?`, password => {
+//     console.log(`Your password is ${password}!`)
+//     readline.close()
+//   })
+
+
+
+const inquirer = require("inquirer");
+
+const question = [
+    {
+        type: "input",
+        name: "name",
+        massage: "What's our name?",
+    },
+    {
+        type: "password",
+        name: "password",
+        massage: "What's our password?"
+    },
+];
+
+inquirer.prompt(question).then((answers) => {
+    console.log(`Your name is ${answers.name}!`);
+    console.log(`Your password is ${answers.password}!`);
+})
