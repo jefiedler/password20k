6cef8496cd77028fa2032ad3cc4c6a0b64fd7283
jefiedler <jonas.fiedler@magenta.de>
2020-08-24 22:47:46 +0200

    new password programm

--- cli.js
+++ cli.js
@@ -1,37 +1,29 @@
-const inquirer = require("inquirer");
-const fs = require("fs").promises;
+const { askQuestionStart, CHOICE_GET, askQuestionGet, CHOICE_SET, askQuestionSet } = require("./src/questions");
+const { readPassword } = require("./src/password");
 
-const question = [
-    {
-        type: "password",
-        name: "password",
-        massage: "What's our master password?"
-    },
-    {
-        type: "input",
-        name: "key",
-        massage: "What's our name?",
-    },
-];
-
-
-
-
-inquirer.prompt(question).then( async (answers) => {
-    console.log(`Your password is ${answers.password}!`);
-    console.log(`You like to know the password of ${answers.key}!`);
-    
-    if (answers.password === "1234") {
-        console.log("Master Password is correct!");
-        try {
-            const passwordJSON =  await fs.readFile('./password.json', 'utf8');
-            const password = JSON.parse(passwordJSON);
-            console.log(`Your ${answers.key} password is ${password[answers.key]}`);
-        } catch (error) {
-            console.error("Somthing went wrong!");
-            console.log(`You like to know the password of ${answers.key}!`);
+async function main (){
+    const {masterPassword, action} = await askQuestionStart();
+    if (masterPassword === "123"){
+        console.log("Password is correct!");
+        if (action === CHOICE_GET) {
+            console.log("Now Get a password");
+            const {key} = await askQuestionGet();
+            try {
+                const password = await readPassword(key);
+                console.log(`Your ${key} password is ${password}`);
+            } catch (error) {
+                console.error("Somthing went wrong");
+            }
+        } else if (action === CHOICE_SET) {
+            console.log("Now Set a password");
+            const {key, password} = await askQuestionSet();
+            console.log(`New Password: ${key} = ${password}`);
         }
     } else {
         console.log("Master Password is incorrect!");
-    };
-})
+    }
+}
+main();
+
+
+
