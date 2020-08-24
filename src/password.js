6ae1208bd5a6e177afae4d71750a65bb719e5b89
jefiedler <jonas.fiedler@magenta.de>
2020-08-24 22:10:56 +0200

    Fix typos in password.js

--- src/password.js
+++ src/password.js
@@ -5,7 +5,7 @@ const fs = require("fs").promises;
 async function readPassword(key) {
     const passwordJson = await fs.readFile("./password.json", "utf-8");
     const passwords = JSON.parse(passwordJson);
-    const password = password[key];
+    const password = passwords[key];
     return password;
 }
 
