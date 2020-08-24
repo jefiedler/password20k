a4c0de41bf5c37aebb830daab1662bc1221dcfcc
jefiedler <jonas.fiedler@magenta.de>
2020-08-24 13:49:12 +0200

    Add get password from seperat datei with fs.readfile

--- cli.js
+++ cli.js
@@ -13,21 +13,39 @@
 
 
 const inquirer = require("inquirer");
+const fs = require("fs").promises;
 
 const question = [
     {
-        type: "input",
-        name: "name",
-        massage: "What's our name?",
-    },
-    {
         type: "password",
         name: "password",
-        massage: "What's our password?"
+        massage: "What's our master password?"
+    },
+    {
+        type: "input",
+        name: "key",
+        massage: "What's our name?",
     },
 ];
 
-inquirer.prompt(question).then((answers) => {
-    console.log(`Your name is ${answers.name}!`);
+
+
+
+inquirer.prompt(question).then( async (answers) => {
     console.log(`Your password is ${answers.password}!`);
+    console.log(`You like to know the password of ${answers.key}!`);
+    
+    if (answers.password === "1234") {
+        console.log("Master Password is correct!");
+        try {
+            const passwordJSON =  await fs.readFile('./password.json', 'utf8');
+            const password = JSON.parse(passwordJSON);
+            console.log(`Your ${answers.key} password is ${password[answers.key]}`);
+        } catch (error) {
+            console.error("Somthing went wrong!");
+            console.log(`You like to know the password of ${answers.key}!`);
+        }
+    } else {
+        console.log("Master Password is incorrect!");
+    };
 })
