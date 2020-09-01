fc50da1b539c9808ce8f5b1e34cf4d9866e0b012
jefiedler <jonas.fiedler@magenta.de>
2020-09-01 17:18:14 +0200

    Add fetch data from api

--- cli/src/App.js
+++ cli/src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
-
 import './App.css';
 
+async function login(){
+  try {
+    const res = await fetch("/api/password/login", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userName: "TestUser",
+        password: "12345"
+      }),
+    });
+    const result = await res.text();
+    alert(result);
+  } catch (error){
+    alert(error.message);
+  }
+};
+
+async function fetchPassword(name){
+  try {
+    const res = await fetch(`/api/passwords/${name}`);
+    const result = await res.text();
+    alert(result);
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
 function App() {
+  const [passwordName, setPasswordName] = React.useState("");
+
   return (
     <div className="App">
-      <h1>Test</h1>
+      <button onClick={login}>Login</button>
+      <label>
+        Password-Name {" "}
+        <input value={passwordName} onChange={(event) => setPasswordName(event.target.value)}/>
+      </label>
+      <button onClick={() => fetchPassword(passwordName)}>Get Password</button>
     </div>
   );
 }
